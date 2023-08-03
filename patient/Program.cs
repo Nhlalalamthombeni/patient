e3a4489c52fc97494910955150e32d09d2967793
Student01 <Student01@DINEO123>
2023-08-03 14:21:19 +0200

    Add project files.

---
+++ patient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+
+namespace patient
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            patient p = new patient();
+            /*p is the object*/ p.Examine("lalah");
+
+            child c = new child();
+            c.Innoculated();
+
+
+
+        }
+        public class patient{
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public int weight { get; set; }
+        public long SSN { get; set; }
+
+
+        public void Examine(string pname)
+        {
+            Console.WriteLine("Examination of " + pname + " completed");
+        }
+        public void Billing(long ssn)
+        {
+            Console.WriteLine("Billing completed...");
+        }
+    }
+        public class child:patient
+        {
+            public void Innoculated()
+            {
+                Console.WriteLine("Child has been innoculated");
+            }
+        }
+    }
+}
